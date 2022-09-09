0727600697eb09735a9d174866c5cac2e9c6d667
mb <benson.moses.bm@gmail.com>
2022-09-08 22:13:50 -0700

    6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char str ="with proper grammar, but the outcome is a piece of art,"
+	printf("%c", str);
+	return (0);
+}
