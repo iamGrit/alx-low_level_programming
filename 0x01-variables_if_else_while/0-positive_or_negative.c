5fbaa219a444ab3ed9f916b8059f8e89726d4f41
mb <benson.moses.bm@gmail.com>
2022-09-09 02:10:30 -0700

    1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*Program prints the last randomly generated number*/
+
+/**
+* main - entry point
+* Return: Always 0 (successful)
+*/
+int main(void)
+{
+
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int ld = n % 10;
+printf("Last digit of %d is %d and is ", n, ld);
+if (ld == 0)
+{
+puts("zero");
+}
+else if (n % 10 > 5)
+{
+puts("and is greater than 5");
+}
+else if (ld < 6)
+{
+puts("less than 6 and not 0");
+}
+return (0);
+}
