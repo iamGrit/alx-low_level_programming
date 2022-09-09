b6b3c71f0cf458b9df2428f0f19c8285aeb8de7a
mb <benson.moses.bm@gmail.com>
2022-09-09 14:24:28 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*Program prints the alphabets*/
+
+/**
+* main - entry point
+* Return: Always 0 (successful)
+*/
+int main(void)
+{
+int x;
+
+for (x = a; x <= z; x++)
+putchar(x);
+putchar('\n');
+}
+return (0);
+}
