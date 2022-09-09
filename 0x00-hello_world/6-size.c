73ba564ed4eab0c398d8f8ef967bc330f2cc4af7
mb <benson.moses.bm@gmail.com>
2022-09-08 23:09:31 -0700

    71

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
  */
 int main (void)
 {
-  int q = sizeof (char);
-  int a = sizeof (int);
-  int b = sizeof (long int);
-  int c = sizeof (long long int);
-  int d = sizeof (float);
+  int q = sizeof(char);
+  int a = sizeof(int);
+  int b = sizeof(long int);
+  int c = sizeof(long long int);
+  int d = sizeof(float);
 
-  printf ("Size of a char: %d byte(s)\n", q);
-  printf ("Size of a int: %d byte(s)\n", a);
-  printf ("Size of a long int: %d byte(s)\n", b);
-  printf ("Size of a long long int: %d byte(s)\n", c);
-  printf ("Size of a float: %d byte(s)\n", d);
-  return (0);
+  printf("Size of a char: %d byte(s)\n", q);
+  printf("Size of an int: %d byte(s)\n", a);
+  printf("Size of a long int: %d byte(s)\n", b);
+  printf("Size of a long long int: %d byte(s)\n", c);
+  printf("Size of a float: %d byte(s)\n", d);
+  return(0);
 }
